3e536ec13dae37d331e90a4019fc7a927645bb7b
Josh <50963558+JoshhSandhu@users.noreply.github.com>
2025-08-29 14:46:24 +0530

    updating the frontend

--- voting-test/anchor/src/counter-exports.ts
+++ voting-test/anchor/src/counter-exports.ts
@@ -1,18 +1,18 @@
 // Here we export some useful types and functions for interacting with the Anchor program.
 import { AnchorProvider, Program } from '@coral-xyz/anchor'
 import { Cluster, PublicKey } from '@solana/web3.js'
-import CounterIDL from '../target/idl/counter.json'
-import type { Counter } from '../target/types/counter'
+import CounterIDL from '../target/idl/voting.json'
+import type { Voting } from '../target/types/voting'
 
 // Re-export the generated IDL and type
-export { Counter, CounterIDL }
+export { Voting, CounterIDL }
 
 // The programId is imported from the program IDL.
 export const COUNTER_PROGRAM_ID = new PublicKey(CounterIDL.address)
 
 // This is a helper function to get the Counter Anchor program.
-export function getCounterProgram(provider: AnchorProvider, address?: PublicKey): Program<Counter> {
-  return new Program({ ...CounterIDL, address: address ? address.toBase58() : CounterIDL.address } as Counter, provider)
+export function getCounterProgram(provider: AnchorProvider, address?: PublicKey): Program<Voting> {
+  return new Program({ ...CounterIDL, address: address ? address.toBase58() : CounterIDL.address } as Voting, provider)
 }
 
 // This is a helper function to get the program ID for the Counter program depending on the cluster.
